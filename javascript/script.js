6321a87a0e37f7f6043aec5fea99c38fce35ba1a
Rametse-Clement <241176@virtualwindow.co.za>
2024-06-12 19:36:32 +0200

    commit
    commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,12 @@
 // scripts.js
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form from submitting normally
+    event.preventDefault(); 
 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const rememberMe = document.getElementById('rememberMe').checked;
 
-    // Perform your login logic here, e.g., sending the data to a server
+    
     console.log('Email:', email);
     console.log('Password:', password);
     console.log('Remember Me:', rememberMe);
